7320da80d1768b661d3f86571826324ec6d83b6f
Justin Nel <justin@cyber-lane.com>
2026-02-01 21:30:48 +0900

    feat: added tmux config

---
+++ run_once_after_install-tmux-tpm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Install TPM if it's not already there
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+    echo "Installing Tmux Plugin Manager..."
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
